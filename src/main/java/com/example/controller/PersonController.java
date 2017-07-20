47d1d9b4998ea36becdb852130b9a763f6a59aa3
ashraf <ashraf@buzzhero.io>
2017-07-20 10:30:04 -0700

    gg

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -1,46 +1,52 @@
 package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.controller.ListLiveChatMessages;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeScopes;
+import com.google.api.services.youtube.model.LiveChatMessage;
+import com.google.api.services.youtube.model.LiveChatMessageAuthorDetails;
+import com.google.api.services.youtube.model.LiveChatMessageListResponse;
+import com.google.api.services.youtube.model.LiveChatMessageSnippet;
+import com.google.api.services.youtube.model.LiveChatSuperChatDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.model.Person;
-import com.example.service.PersonService;
-
-import java.util.Map;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 @Controller
+@RequestMapping("/index")
 public class PersonController {
 
-    @Autowired
-    private PersonService personService;
 
-    @RequestMapping("/")
-    public String listPeople(Map<String, Object> map) {
+    private static YouTube youtube;
 
-        map.put("person", new Person());
-        map.put("peopleList", personService.listPeople());
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model, @RequestParam(value = "videoId", required = false) String videoId) {
 
-        return "people";
+     /*   ListLiveChatMessages listLiveChatMessages = new ListLiveChatMessages();
+        List<LiveChatMessage> liveChatMessages = new ArrayList<LiveChatMessage>();
+        //Youtubesearch youtubesearch = new Youtubesearch();
+
+        try {
+            liveChatMessages  = listLiveChatMessages.grabLiveMessages(videoId);
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        model.put("videoId", videoId);
+        model.put("liveMessages", liveChatMessages);
+        */
+        return "index";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addPerson(@ModelAttribute("person") Person person, BindingResult result) {
 
-        personService.addPerson(person);
-
-        return "redirect:/people/";
-    }
-
-    @RequestMapping("/delete/{personId}")
-    public String deletePerson(@PathVariable("personId") Integer personId) {
-
-        personService.removePerson(personId);
-
-        return "redirect:/people/";
-    }
 }
